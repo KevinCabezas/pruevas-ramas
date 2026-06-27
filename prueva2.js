583218166749b6fb805650ff661adc1333545837
KevinCabezas <kevincabesuchi@gmail.com>
2026-06-26 23:28:27 -0300

    Update prueva2

--- prueva2.js
+++ prueva2.js
@@ -1,5 +1,12 @@
 // cree una nueva rama con un usuario colaborador usando
 // git switch -c nombre-rama
 
+// prepare lo cambios y los guarde en mi repo local
+// git add
+// git commit -m "Mensaje descriptivo"
+
+// subi los cambnios a la rama secundaria para que los tengan otros 
+// git push -u origin nombre-rama
+
 
 console.log('hola mundo');
